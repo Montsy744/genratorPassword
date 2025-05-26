0e0b98820aa1abdba0780b9ba9d34fa1c45b223a
Hugo Straseele <straseelehugo@gmail.com>
2025-05-26 20:01:06 +0200

    update

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+function generatePassword() {
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+  const length = 12;
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+  document.getElementById("password").value = password;
+}
